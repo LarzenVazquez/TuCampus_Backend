2988f5b307181cfee2e800e7ba0e65eac796df41
AndreaVasquez <andyy_vasquez_2102@hotmail.com>
2026-04-12 21:18:42 -0600

    yarailwayyaaaaaa

--- src/api/sql-service/controllers/authController.js
+++ src/api/sql-service/controllers/authController.js
@@ -19,12 +19,16 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, 
   },
-  family: 4,
+  tls: {
+    rejectUnauthorized: false 
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 /* Registro con hashing Bcrypt */
