d4c645195a84d01896d4b45abadbdfe9f508e549
AndreaVasquez <andyy_vasquez_2102@hotmail.com>
2026-04-12 21:07:52 -0600

    port587

--- src/api/sql-service/controllers/authController.js
+++ src/api/sql-service/controllers/authController.js
@@ -17,8 +17,9 @@ const getPublicKeyEndpoint = (req, res) => {
 // Configuración corregida para evitar el error ENETUNREACH en Railway
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, 
