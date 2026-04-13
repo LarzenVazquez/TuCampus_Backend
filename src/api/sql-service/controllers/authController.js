c4dfe6a0d8e7f3325bf855b7d0ebfafc45a7ef9b
AndreaVasquez <andyy_vasquez_2102@hotmail.com>
2026-04-12 20:52:47 -0600

    family4

--- src/api/sql-service/controllers/authController.js
+++ src/api/sql-service/controllers/authController.js
@@ -23,6 +23,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, 
   },
+  family: 4,
 });
 
 /* Registro con hashing Bcrypt */
